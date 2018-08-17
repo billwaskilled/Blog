95d6d0d69d834349588e731b1b78ba0921df628b
changyingjie <384488069@qq.com>
2018-08-17 10:01:36 +0800

    优化注册检查逻辑

--- src/main/java/example/service/RegisterCheck.java
+++ src/main/java/example/service/RegisterCheck.java
@@ -5,11 +5,15 @@ import example.service.utils.AppMD5Util;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class RegisterCheck {
     public static boolean register(String username,String password,String age){
         boolean isSuccess = false;
-        boolean isPassed = LoginCheck.check(username, password);
+        if (username.length() <= 3 || username.length() >= 15) return isSuccess;     // 如果username长度不符合 4 到 14个字符，报错返回
+        if (password.length() <= 3 || password.length() >= 15) return isSuccess;     // 如果password长度不符合 4 到 14个字符，报错返回
+
+        boolean isPassed = checkUsernameIsExisting(username);
 
         password = AppMD5Util.getMD5(password);
 
@@ -29,5 +33,25 @@ public class RegisterCheck {
             }
         }
         return isSuccess;
+    }
+
+    private static boolean checkUsernameIsExisting(String username) {
+        boolean isExisting = false;
+        try {
+            Connection conn = Dao.getConnection();
+            PreparedStatement p = conn.prepareStatement("select * from user where username=?");
+            p.setString(1, username);
+            ResultSet rs = p.executeQuery();
+            if (rs.next()){
+                isExisting = true;
+            } else {
+                isExisting = false;
+            }
+            Dao.close(rs, p, conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return isExisting;
     }
 }
